62e42652d1be3b472d75fa1ed0035e8dbf72272d
Rehab Hamdy <209379697+rehab-hamdi-selim@users.noreply.github.com>
2025-05-13 23:44:50 +0300

    App Assets

---
+++ quran_app/lib/core/assets/app_images.dart
@@ -0,0 +1,28 @@
+class AppImages {
+  static const String tasbeehImage = "assets/images/tasbeeh.png";
+  static const String splashScreenLight =
+      "assets/images/splashScreenLight.jpeg";
+  static const String splashScreenDark = "assets/images/splashScreenDark.jpeg";
+
+  static const String backgroundLight = "assets/images/backgroundLight.jpeg";
+  static const String backgroundDark = "assets/images/backgroundDark.jpeg";
+
+  //prayer times
+  static const String fogImage = "assets/images/prayerTimesIcons/fog.png";
+  static const String sunImage = "assets/images/prayerTimesIcons/sun.png";
+  static const String moonImage = "assets/images/prayerTimesIcons/moon.png";
+  static const String cloudImage = "assets/images/prayerTimesIcons/cloud.png";
+  static const String sunriseImage =
+      "assets/images/prayerTimesIcons/sunrise.png";
+  static const String sunsetImage = "assets/images/prayerTimesIcons/sunset.png";
+
+  //bottom navigation bar
+  static const String hadithImage =
+      "assets/images/bottomNavigationBarIcons/hadith.png";
+  static const String quranImage =
+      "assets/images/bottomNavigationBarIcons/quran.png";
+  static const String mosqueImage =
+      "assets/images/bottomNavigationBarIcons/mosque.png";
+  static const String sephaImage =
+      "assets/images/bottomNavigationBarIcons/sepha.png";
+}
